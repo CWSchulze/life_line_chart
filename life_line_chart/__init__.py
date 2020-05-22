06ca637f0dbb75dee6253f9a0030c33bab71cf8a
Christian Schulze <c.w.schulze@gmail.com>
2020-05-22 15:15:06 +0200

    1.3.2

--- life_line_chart/__init__.py
+++ life_line_chart/__init__.py
@@ -23,7 +23,7 @@ from .InstanceContainer import InstanceContainer
 
 from .Exceptions import LifeLineChartCannotMoveIndividual, LifeLineChartCollisionDetected, LifeLineChartNotEnoughInformationToDisplay
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 logging.basicConfig()
 logger = logging.getLogger("life_line_chart")
