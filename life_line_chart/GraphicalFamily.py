aeadf5efa19b55a7588262160e6c9bff45f29be6
Christian Schulze <c.w.schulze@gmail.com>
2020-05-31 13:00:10 +0200

    gr link is now implemented as property/connection

--- life_line_chart/GraphicalFamily.py
+++ life_line_chart/GraphicalFamily.py
@@ -14,9 +14,6 @@ class GraphicalFamily():
         self.family.graphical_representations.append(self)
         self.marriage = self.family.marriage
 
-        self.gr_husb = None
-        self.gr_wife = None
-
         # Tells which children are visible
         self.visible_children = []
 
@@ -106,6 +103,65 @@ class GraphicalFamily():
             if self.g_id not in self.__instances.connection_container['i'][gr_child.g_id]:
                 self.__instances.connection_container['i'][gr_child.g_id][self.g_id] = []
             self.__instances.connection_container['i'][gr_child.g_id][self.g_id].append("strong_child")
+
+
+    @property
+    def gr_husb(self):
+        if self.g_id not in self.__instances.connection_container['f']:
+            return None
+        gr_husbs = []
+        for g_id, connections in self.__instances.connection_container['f'][self.g_id].items():
+            if 'gr_husb' in connections:
+                gr_husbs.append(self.__instances[('i', g_id[1])].graphical_representations[g_id[0]])
+        if len(gr_husbs) > 1:
+            raise RuntimeError("Something went wrong in the placement algorithm")
+        elif len(gr_husbs) > 0:
+            return gr_husbs[0]
+        return None
+
+    @gr_husb.setter
+    def gr_husb(self, gr_husb):
+        if gr_husb != None:
+            if self.g_id not in self.__instances.connection_container['f']:
+                self.__instances.connection_container['f'][self.g_id] = {}
+            if gr_husb.g_id not in self.__instances.connection_container['f'][self.g_id]:
+                self.__instances.connection_container['f'][self.g_id][gr_husb.g_id] = []
+            self.__instances.connection_container['f'][self.g_id][gr_husb.g_id].append("gr_husb")
+
+            if gr_husb.g_id not in self.__instances.connection_container['i']:
+                self.__instances.connection_container['i'][gr_husb.g_id] = {}
+            if self.g_id not in self.__instances.connection_container['i'][gr_husb.g_id]:
+                self.__instances.connection_container['i'][gr_husb.g_id][self.g_id] = []
+            self.__instances.connection_container['i'][gr_husb.g_id][self.g_id].append("gr_husb")
+
+    @property
+    def gr_wife(self):
+        if self.g_id not in self.__instances.connection_container['f']:
+            return None
+        gr_wifes = []
+        for g_id, connections in self.__instances.connection_container['f'][self.g_id].items():
+            if 'gr_wife' in connections:
+                gr_wifes.append(self.__instances[('i', g_id[1])].graphical_representations[g_id[0]])
+        if len(gr_wifes) > 1:
+            raise RuntimeError("Something went wrong in the placement algorithm")
+        elif len(gr_wifes) > 0:
+            return gr_wifes[0]
+        return None
+
+    @gr_wife.setter
+    def gr_wife(self, gr_wife):
+        if gr_wife != None:
+            if self.g_id not in self.__instances.connection_container['f']:
+                self.__instances.connection_container['f'][self.g_id] = {}
+            if gr_wife.g_id not in self.__instances.connection_container['f'][self.g_id]:
+                self.__instances.connection_container['f'][self.g_id][gr_wife.g_id] = []
+            self.__instances.connection_container['f'][self.g_id][gr_wife.g_id].append("gr_wife")
+
+            if gr_wife.g_id not in self.__instances.connection_container['i']:
+                self.__instances.connection_container['i'][gr_wife.g_id] = {}
+            if self.g_id not in self.__instances.connection_container['i'][gr_wife.g_id]:
+                self.__instances.connection_container['i'][gr_wife.g_id][self.g_id] = []
+            self.__instances.connection_container['i'][gr_wife.g_id][self.g_id].append("gr_wife")
 
 
     @property
