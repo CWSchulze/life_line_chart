65703f55f42ee745abaffa731eb47c24c5ac51ba
Christian Schulze <c.w.schulze@gmail.com>
2020-06-02 23:22:02 +0200

    simplify

--- tests/test_svg.py
+++ tests/test_svg.py
@@ -36,8 +36,7 @@ def test_generate_svg_file():
         child_of_family = chart._instances[('f', cof_ids[0])]
     chart.place_selected_individuals(
         gr_root_individual, None, None, child_of_family, x_position)
-    x_min, x_max = chart._instances[('i', individual_id)
-                             ].graphical_representations[0].get_ancestor_range(None)
+    x_min, x_max = gr_root_individual.get_ancestor_range(None)
     x_position += x_max - x_min + 1
 
     chart.modify_layout(individual_id)
